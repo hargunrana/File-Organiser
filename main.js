8ba37e63684e7f34c8f6753f6313a4e81baabc66
hargunrana <hargun.rana1@gmail.com>
2022-03-26 13:50:23 +0530

    Fixed path issue in main.js

--- main.js
+++ main.js
@@ -3,22 +3,28 @@
 let helpFunc = require("./commands/help");
 let organiseFunc = require("./commands/organise");
 let treeFunc = require("./commands/tree");
+
 let inputArr = process.argv.slice(2);
+
 let command = inputArr[0];
 let path = inputArr[1];
+
 switch (command) {
     case "tree":
         //call tree Function
         treeFunc.tree(path);
         break;
+
     case "organise":
         //call organise Function
         organiseFunc.organise(path);
         break;
+
     case "help":
         //call help function
         helpFunc.help();
         break;
+
     default:
         console.log("Command not recognised :/");
         break;
